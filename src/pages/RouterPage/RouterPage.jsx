22406cf5c58d1fac88780f9f743c5c2ff8b67e4c
jamshidbekmirzakarimov <jamshidbekmirzakarimov6@gmail.com>
2024-12-15 11:30:13 +0500

    fixed

--- src/pages/RouterPage/RouterPage.jsx
+++ src/pages/RouterPage/RouterPage.jsx
@@ -12,8 +12,7 @@ const RouterPage = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/admin" element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="books" element={<Books />} />
